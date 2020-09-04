3495df0f3672cc49a8b1946f089598ce2c33e273
student <student@C251U27>
2020-09-04 12:03:03 +0300

    1

--- PR01-02/PR01-02/PR01-02/Program.cs
+++ PR01-02/PR01-02/PR01-02/Program.cs
@@ -2,25 +2,28 @@
 
 namespace PR01_02
 {
-  static void Main(string[] args)
+  class Exception
   {
-    SByte a = 0;
-    Byte b = 0;
-    Int16 с = 0;
-    Int32 d = 0;
-    Int64 e = 0;
-    string s = "";
-    Exception ex = new Exception();
-
-    object[] types = { a, b, c, d, e, s, ex };
-
-    foreach (object о in types)
+ 
+    static void Main(string[] args)
     {
-      string type;
-      if (o.GetType().IsValueType) type = "Value type";
-      else
-        type = "Reference Type";
-      Console.WriteLine("{0}: {1}", o.GetType(), type);
+      SByte a = 0;
+      Byte b = 0;
+      Int16 c = 0;
+      Int32 d = 0;
+      Int64 e = 0;
+      string s = "";
+      Exception ex = new Exception();
+      object[] types = { a, b, c, d, e, s, ex };
+
+      foreach (object o in types)
+      {
+        string type;
+        if (o.GetType().IsValueType) type = "Value type";
+        else
+          type = "Reference Type";
+        Console.WriteLine("{0}: {1}", o.GetType(), type);
+      }
     }
   }
 }
