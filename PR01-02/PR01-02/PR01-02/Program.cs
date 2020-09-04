ee87fe21a9f1ad176c9be7bb3dedbcc6947009e0
student <student@C251U27>
2020-09-04 10:49:34 +0300

    1

--- PR01-02/PR01-02/PR01-02/Program.cs
+++ PR01-02/PR01-02/PR01-02/Program.cs
@@ -2,11 +2,11 @@
 
 namespace PR01_02
 {
-  class Program
-  {
-    static void Main(string[] args)
-    {
-      Console.WriteLine("Hello World!");
-    }
-  }
+  SByte a = 0; 
+  Byte b = 0; 
+  Int16 с =0; 
+  Int32 d = 0; 
+  Int64 e = 0; 
+  string s = "";
+  Exception ex = new Exception();
 }
