f06eacf121c254e7fef167940f0ff395fc73c74a
student <student@C251U27>
2020-09-04 11:16:11 +0300

    1

--- PR01-01/PR01-01/Program.cs
+++ PR01-01/PR01-01/Program.cs
@@ -8,6 +8,7 @@ namespace PR01_01
     public string firstName;
     public string lastName;
     public int age;
+    public enum Genders : int { Male, Female };
 
     public Person(string _firstName, string _lastName, int _age)
     {
